7ca2951c933db45b137b73ee6a123932c42a1486
SeaHi-Mo <seahi@foxmail.com>
2023-07-24 15:00:26 +0800

    update bt

--- components/wireless/bluetooth/blestack/src/services/dis.c
+++ components/wireless/bluetooth/blestack/src/services/dis.c
@@ -2,13 +2,13 @@
  *  @brief GATT Device Information Service
  */
 
-/*
- * Copyright (c) 2019 Demant
- * Copyright (c) 2018 Nordic Semiconductor ASA
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+ /*
+  * Copyright (c) 2019 Demant
+  * Copyright (c) 2018 Nordic Semiconductor ASA
+  * Copyright (c) 2016 Intel Corporation
+  *
+  * SPDX-License-Identifier: Apache-2.0
+  */
 
 #include <zephyr/types.h>
 #include <stddef.h>
@@ -44,6 +44,8 @@ struct dis_pnp {
 #define CONFIG_BT_GATT_DIS_MODEL    "BL702_BLE_MODEL"
 #elif defined(BL602)
 #define CONFIG_BT_GATT_DIS_MODEL    "BL602_BLE_MODEL"
+#elif defined(BL616)
+#define CONFIG_BT_GATT_DIS_MODEL    "BL61x_BLE_MODEL"
 #else
 #define CONFIG_BT_GATT_DIS_MODEL
 #endif
@@ -72,19 +74,19 @@ static u8_t dis_model[CONFIG_BT_GATT_DIS_STR_MAX] = CONFIG_BT_GATT_DIS_MODEL;
 static u8_t dis_manuf[CONFIG_BT_GATT_DIS_STR_MAX] = CONFIG_BT_GATT_DIS_MANUF;
 #if defined(CONFIG_BT_GATT_DIS_SERIAL_NUMBER)
 static u8_t dis_serial_number[CONFIG_BT_GATT_DIS_STR_MAX] =
-	CONFIG_BT_GATT_DIS_SERIAL_NUMBER_STR;
+CONFIG_BT_GATT_DIS_SERIAL_NUMBER_STR;
 #endif
 #if defined(CONFIG_BT_GATT_DIS_FW_REV)
 static u8_t dis_fw_rev[CONFIG_BT_GATT_DIS_STR_MAX] =
-	CONFIG_BT_GATT_DIS_FW_REV_STR;
+CONFIG_BT_GATT_DIS_FW_REV_STR;
 #endif
 #if defined(CONFIG_BT_GATT_DIS_HW_REV)
 static u8_t dis_hw_rev[CONFIG_BT_GATT_DIS_STR_MAX] =
-	CONFIG_BT_GATT_DIS_HW_REV_STR;
+CONFIG_BT_GATT_DIS_HW_REV_STR;
 #endif
 #if defined(CONFIG_BT_GATT_DIS_SW_REV)
 static u8_t dis_sw_rev[CONFIG_BT_GATT_DIS_STR_MAX] =
-	CONFIG_BT_GATT_DIS_SW_REV_STR;
+CONFIG_BT_GATT_DIS_SW_REV_STR;
 #endif
 
 #define BT_GATT_DIS_MODEL_REF			dis_model
@@ -105,8 +107,8 @@ static u8_t dis_sw_rev[CONFIG_BT_GATT_DIS_STR_MAX] =
 
 #endif /* CONFIG_BT_GATT_DIS_SETTINGS */
 
-static ssize_t read_str(struct bt_conn *conn,
-			  const struct bt_gatt_attr *attr, void *buf,
+static ssize_t read_str(struct bt_conn* conn,
+			  const struct bt_gatt_attr* attr, void* buf,
 			  u16_t len, u16_t offset)
 {
 	return bt_gatt_attr_read(conn, attr, buf, len, offset, attr->user_data,
@@ -114,8 +116,8 @@ static ssize_t read_str(struct bt_conn *conn,
 }
 
 #if CONFIG_BT_GATT_DIS_PNP
-static ssize_t read_pnp_id(struct bt_conn *conn,
-			   const struct bt_gatt_attr *attr, void *buf,
+static ssize_t read_pnp_id(struct bt_conn* conn,
+			   const struct bt_gatt_attr* attr, void* buf,
 			   u16_t len, u16_t offset)
 {
 	return bt_gatt_attr_read(conn, attr, buf, len, offset, &dis_pnp_id,
@@ -124,42 +126,42 @@ static ssize_t read_pnp_id(struct bt_conn *conn,
 #endif
 
 /* Device Information Service Declaration */
-static struct bt_gatt_attr attrs[]= {
+static struct bt_gatt_attr attrs[] = {
 
 	BT_GATT_PRIMARY_SERVICE(BT_UUID_DIS),
 
 	BT_GATT_CHARACTERISTIC(BT_UUID_DIS_MODEL_NUMBER,
-			       BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
-			       read_str, NULL, BT_GATT_DIS_MODEL_REF),
+				   BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
+				   read_str, NULL, BT_GATT_DIS_MODEL_REF),
 	BT_GATT_CHARACTERISTIC(BT_UUID_DIS_MANUFACTURER_NAME,
-			       BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
-			       read_str, NULL, BT_GATT_DIS_MANUF_REF),
+				   BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
+				   read_str, NULL, BT_GATT_DIS_MANUF_REF),
 #if CONFIG_BT_GATT_DIS_PNP
 	BT_GATT_CHARACTERISTIC(BT_UUID_DIS_PNP_ID,
-			       BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
-			       read_pnp_id, NULL, &dis_pnp_id),
+				   BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
+				   read_pnp_id, NULL, &dis_pnp_id),
 #endif
 
 #if defined(CONFIG_BT_GATT_DIS_SERIAL_NUMBER)
 	BT_GATT_CHARACTERISTIC(BT_UUID_DIS_SERIAL_NUMBER,
-			       BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
-			       read_str, NULL,
-			       BT_GATT_DIS_SERIAL_NUMBER_STR_REF),
+				   BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
+				   read_str, NULL,
+				   BT_GATT_DIS_SERIAL_NUMBER_STR_REF),
 #endif
 #if defined(CONFIG_BT_GATT_DIS_FW_REV)
 	BT_GATT_CHARACTERISTIC(BT_UUID_DIS_FIRMWARE_REVISION,
-			       BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
-			       read_str, NULL, BT_GATT_DIS_FW_REV_STR_REF),
+				   BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
+				   read_str, NULL, BT_GATT_DIS_FW_REV_STR_REF),
 #endif
 #if defined(CONFIG_BT_GATT_DIS_HW_REV)
 	BT_GATT_CHARACTERISTIC(BT_UUID_DIS_HARDWARE_REVISION,
-			       BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
-			       read_str, NULL, BT_GATT_DIS_HW_REV_STR_REF),
+				   BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
+				   read_str, NULL, BT_GATT_DIS_HW_REV_STR_REF),
 #endif
 #if defined(CONFIG_BT_GATT_DIS_SW_REV)
 	BT_GATT_CHARACTERISTIC(BT_UUID_DIS_SOFTWARE_REVISION,
-			       BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
-			       read_str, NULL, BT_GATT_DIS_SW_REV_STR_REF),
+				   BT_GATT_CHRC_READ, BT_GATT_PERM_READ,
+				   read_str, NULL, BT_GATT_DIS_SW_REV_STR_REF),
 #endif
 
 };
@@ -168,27 +170,28 @@ static struct bt_gatt_service dis_svc = BT_GATT_SERVICE(attrs);
 
 void dis_init(u8_t vid_src, u16_t vid, u16_t pid, u16_t pid_ver)
 {
-    dis_pnp_id.pnp_vid_src = vid_src;
-    dis_pnp_id.pnp_vid = vid;
-    dis_pnp_id.pnp_pid = pid;
-    dis_pnp_id.pnp_ver = pid_ver;
+	dis_pnp_id.pnp_vid_src = vid_src;
+	dis_pnp_id.pnp_vid = vid;
+	dis_pnp_id.pnp_pid = pid;
+	dis_pnp_id.pnp_ver = pid_ver;
 	bt_gatt_service_register(&dis_svc);
 }
 
 #if defined(CONFIG_BT_SETTINGS) && defined(CONFIG_BT_GATT_DIS_SETTINGS)
-static int dis_set(const char *name, size_t len_rd,
-		   settings_read_cb read_cb, void *store)
+static int dis_set(const char* name, size_t len_rd,
+		   settings_read_cb read_cb, void* store)
 {
 	int len, nlen;
-	const char *next;
+	const char* next;
 
 	nlen = settings_name_next(name, &next);
 	if (!strncmp(name, "manuf", nlen)) {
 		len = read_cb(store, &dis_manuf, sizeof(dis_manuf) - 1);
 		if (len < 0) {
 			BT_ERR("Failed to read manufacturer from storage"
-				       " (err %d)", len);
-		} else {
+					   " (err %d)", len);
+		}
+		else {
 			dis_manuf[len] = '\0';
 
 			BT_DBG("Manufacturer set to %s", dis_manuf);
@@ -199,8 +202,9 @@ static int dis_set(const char *name, size_t len_rd,
 		len = read_cb(store, &dis_model, sizeof(dis_model) - 1);
 		if (len < 0) {
 			BT_ERR("Failed to read model from storage"
-				       " (err %d)", len);
-		} else {
+					   " (err %d)", len);
+		}
+		else {
 			dis_model[len] = '\0';
 
 			BT_DBG("Model set to %s", dis_model);
@@ -213,8 +217,9 @@ static int dis_set(const char *name, size_t len_rd,
 			   sizeof(dis_serial_number) - 1);
 		if (len < 0) {
 			BT_ERR("Failed to read serial number from storage"
-				       " (err %d)", len);
-		} else {
+					   " (err %d)", len);
+		}
+		else {
 			dis_serial_number[len] = '\0';
 
 			BT_DBG("Serial number set to %s", dis_serial_number);
@@ -227,8 +232,9 @@ static int dis_set(const char *name, size_t len_rd,
 		len = read_cb(store, &dis_fw_rev, sizeof(dis_fw_rev) - 1);
 		if (len < 0) {
 			BT_ERR("Failed to read firmware revision from storage"
-				       " (err %d)", len);
-		} else {
+					   " (err %d)", len);
+		}
+		else {
 			dis_fw_rev[len] = '\0';
 
 			BT_DBG("Firmware revision set to %s", dis_fw_rev);
@@ -241,8 +247,9 @@ static int dis_set(const char *name, size_t len_rd,
 		len = read_cb(store, &dis_hw_rev, sizeof(dis_hw_rev) - 1);
 		if (len < 0) {
 			BT_ERR("Failed to read hardware revision from storage"
-				       " (err %d)", len);
-		} else {
+					   " (err %d)", len);
+		}
+		else {
 			dis_hw_rev[len] = '\0';
 
 			BT_DBG("Hardware revision set to %s", dis_hw_rev);
@@ -255,8 +262,9 @@ static int dis_set(const char *name, size_t len_rd,
 		len = read_cb(store, &dis_sw_rev, sizeof(dis_sw_rev) - 1);
 		if (len < 0) {
 			BT_ERR("Failed to read software revision from storage"
-				       " (err %d)", len);
-		} else {
+					   " (err %d)", len);
+		}
+		else {
 			dis_sw_rev[len] = '\0';
 
 			BT_DBG("Software revision set to %s", dis_sw_rev);
